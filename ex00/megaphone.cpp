4db6743af46e4b57dd7b924b9d3426e5ecb55df0
Abdellah Benzahour <abde.benz290@gmail.com>
2025-09-16 15:33:52 +0100

    ex00

---
+++ ex00/megaphone.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(int ac, char **av)
+{
+	for (int i = 1; av[i]; i++)
+	{
+		for (int j = 0; av[i][j]; j++)
+			av[i][j] = std::toupper(av[i][j]);
+		std::cout << av[i];
+	}
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *"; 
+	std::cout << std::endl;
+}
